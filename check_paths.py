102b20222994577442cfafac684fc2696e33d78a
Yihan Wu <yihanwucreate@gmail.com>
2026-04-01 18:24:21 -0700

    Delete check_paths.py

--- check_paths.py
+++
@@ -1,6 +0,0 @@
-import os
-print("Current Directory:", os.getcwd())
-print("Files here:", os.listdir())
-if os.path.exists("evaluation"):
-    print("Found evaluation folder!")
-    print("Inside evaluation:", os.listdir("evaluation"))
